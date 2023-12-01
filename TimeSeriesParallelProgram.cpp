ca97297875876998765c2d1367683a6f8ed3a694
FisherZE <125408455+FisherZE@users.noreply.github.com>
2023-12-01 23:15:39 +0000

    Parallel Time Series

--- TimeSeriesParallelProgram.cpp
+++ TimeSeriesParallelProgram.cpp
@@ -13,12 +13,25 @@ vector<double> createSeries(string fileName, vector<double> data);
 
 vector<double> parseTemplate(string fileName, vector<double> data);
 
-int matchTemplate(vector<double> series, vector<double> temp);
+vector<int> matchTemplate(vector<double> series, vector<double> temp, int local_x, int work);
 
 int main(int argc, char* argv[]){
-    if(argc != 3){
-        cout<< "Correct usage: ./project <input_file> <template_file> \n";
-		exit(1);
+    //MPI Initialize
+    int rank, nproc;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+    //Other variables
+    vector<vector<int>> global_Result;
+    int bestPos;
+    int minSAD = 10000000;
+
+    if (argc != 3){
+        if (world_rank == 0)
+            cout << "Correct usage: mpirun -n <num_processes> ./project <input_file> <template_file> \n";
+        MPI_Finalize();
+        exit(1);
     }
 
     //Variable setup
@@ -28,15 +41,49 @@ int main(int argc, char* argv[]){
     vector<double> series;
     vector<double> temp;
 
-    // Create series
-    series = createSeries(fileName, series);
+    if(rank == 0){
+        // Create series
+        series = createSeries(fileName, series);
 
-    //Create template
-    temp = parseTemplate(templateName, temp);
+        //Create template
+        temp = parseTemplate(templateName, temp);
+    }
+
+    MPI_Bcast(&series, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&temp, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     //Match
-    int pos = matchTemplate(series, temp);
-    cout << "Best Position: " << pos << endl;
+    int work = (series.size() - temp.size()) / nproc;
+    int local_x = work * rank;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start = MPI_Wtime();
+
+    vector<int> local_Result = matchTemplate(series, temp, local_x, work);
+
+    // Each process inserts its local vector into the global vector
+    for (int i = 0; i < nproc; ++i) {
+        if (rank == i) {
+            global_Result.push_back(local_Result);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);  // Ensure they wait their turn
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double elapsedTime = MPI_Wtime() - start;
+
+    //Host gets the best vector
+    if(rank == 0){
+        for (int i = 0; i < global_Result.size(); i++){
+            if(global_Result[i][1] < minSAD){
+                minSAD = global_Result[i][1];
+                bestPos = global_Result[i][0];
+            }
+        }
+        cout << "Best Position: " << bestPos << endl;
+        cout << "SAD Value: " << minSAD << endl;
+        cout << "Time: " << elapsedTime << endl;
+    }
 
 }
 
@@ -86,15 +133,18 @@ vector<double> parseTemplate(string fileName, vector<double> data){
     return data;
 }
 
-int matchTemplate(vector<double> series, vector<double> temp){
+vector<int> matchTemplate(vector<double> series, vector<double> temp, int local_x, int work){
     int position = -1;
 
     //Temp max value
-	double minSAD = 10000;
-    double SAD = 0.0;
+	int minSAD = temp.size() * 255;
+    int SAD = 0.0;
+
+    //return value
+    vector<int> result = {position, minSAD};
 
     //Loop through series
-	for(int i = 0; i < series.size() - temp.size(); i++){
+	for(int i = local_x; i < local_x + work; i++){
 		SAD = 0.0;
 		//Loop through template
 		for(int j = 0; j < temp.size(); j++){
@@ -109,5 +159,7 @@ int matchTemplate(vector<double> series, vector<double> temp){
 		}
 	}
     printf("Best SAD: %f\n", minSAD);
-	return position;
+    result[0] = position;
+    result[1] = minSAD;
+	return result;
 }
