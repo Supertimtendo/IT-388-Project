c8e5e62080346189c38cfa124146fa4624d51f22
FisherZE <125408455+FisherZE@users.noreply.github.com>
2023-12-01 20:40:57 +0000

    Quality of life changes, made parallel series

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # IT-388-Project
+
+ImageMatch.cpp - Currently in use, is the implementation file for all Image Template matching methods.
+ImageMatch.h - Currently in use, is the header file for all Image Template matching methods and variables.
+ImageProgram.cpp - Currently in use, is the main that runs the Image Template matching - requires a image and a template image to run.
+
+TimeSeries.cpp - Not in use, Was an implemenation file for the time series matching, all methods were directly inserted into TimeSeriesProgram.cpp for simplicity.
+TimeSeries.h - Not in use, Was the header file, not needed as all required methods were put into the TimeSeriesProgram.cpp.
+TimeSeriesProgram.cpp - Currently in use, contains all methods required for template matching, requires a time series and a template series to run.
+
+TimeSeriesParallelProgram.cpp - Not functional, Will be the parallelized version of the TimeSeriesProgram.cpp.
+
+project.cpp - Not in use, was for having a single file perform all functions, functionalities split among other files and project.cpp is not needed at this time.
+project.h - Same as project.cpp.
---
+++ TimeSeriesParallelProgram.cpp
@@ -0,0 +1,113 @@
+/*
+Parallelized Time Series Program with MPI
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <mpi.h>
+
+using namespace std;
+
+vector<double> createSeries(string fileName, vector<double> data);
+
+vector<double> parseTemplate(string fileName, vector<double> data);
+
+int matchTemplate(vector<double> series, vector<double> temp);
+
+int main(int argc, char* argv[]){
+    if(argc != 3){
+        cout<< "Correct usage: ./project <input_file> <template_file> \n";
+		exit(1);
+    }
+
+    //Variable setup
+    string fileName = argv[1];
+	string templateName = argv[2];
+
+    vector<double> series;
+    vector<double> temp;
+
+    // Create series
+    series = createSeries(fileName, series);
+
+    //Create template
+    temp = parseTemplate(templateName, temp);
+
+    //Match
+    int pos = matchTemplate(series, temp);
+    cout << "Best Position: " << pos << endl;
+
+}
+
+vector<double> createSeries(string fileName, vector<double> data){
+    ifstream infile(fileName);
+    if(infile.fail()){
+        cout << "File could not be opened\n" << endl;
+        exit(1);
+    }
+
+    //string variable
+    string val;
+
+    //While loop to read all values into the series vector
+    while(getline(infile, val))
+    {
+        double num = stod(val);
+        data.push_back(num);
+        cout << num << " ";
+    }
+
+    cout << endl;
+    infile.close();
+    return data;
+}
+
+vector<double> parseTemplate(string fileName, vector<double> data){
+    ifstream infile(fileName);
+    if(infile.fail()){
+        cout << "File could not be opened\n" << endl;
+        exit(1);
+    }
+
+    //string variable
+    string val;
+
+    //While loop to read all values into the series vector
+    while(getline(infile, val))
+    {
+        double num = stod(val);
+        data.push_back(num);
+        cout << num << " ";
+    }
+
+    cout << endl;
+    infile.close();
+    return data;
+}
+
+int matchTemplate(vector<double> series, vector<double> temp){
+    int position = -1;
+
+    //Temp max value
+	double minSAD = 10000;
+    double SAD = 0.0;
+
+    //Loop through series
+	for(int i = 0; i < series.size() - temp.size(); i++){
+		SAD = 0.0;
+		//Loop through template
+		for(int j = 0; j < temp.size(); j++){
+			SAD += abs(series[i+j] - temp[j]);
+		}
+        cout << SAD << endl;
+
+		if(SAD < minSAD){
+			minSAD = SAD;
+			//Stores index start of best matched template (go til length of template)
+			position = i + 1;
+		}
+	}
+    printf("Best SAD: %f\n", minSAD);
+	return position;
+}
